33dd70a0addd99c574351c0f594ba844bdfa67bc
Eslam-shaban <73853163+Eslam-shaban@users.noreply.github.com>
2023-10-07 15:35:36 +0300

    prepare callbacks added

--- src/lung_cancer/config/configuration.py
+++ src/lung_cancer/config/configuration.py
@@ -1,7 +1,7 @@
-
+import os
 from lung_cancer.constants import *
 from lung_cancer.utils.common import read_yaml, create_directories
-from lung_cancer.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from lung_cancer.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
 
 class ConfigurationManager:
     def __init__(self, config_filepath = CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH):
@@ -11,7 +11,7 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root]) # create artifacts directory
 
-    
+    # Data Ingestion Configuration
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -25,10 +25,10 @@ class ConfigurationManager:
             command_api=config.command_api,
             local_data_file_kaggle=config.local_data_file_kaggle
         )
-
         return data_ingestion_config
     
 
+    # Prepare base mode configuration
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
             config = self.config.prepare_base_model
             
@@ -44,5 +44,21 @@ class ConfigurationManager:
                 params_weights=self.params.WEIGHTS,
                 params_classes=self.params.CLASSES
             )
-
             return prepare_base_model_config
+    
+
+    # Prepare callbacks configuration
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath) # return artifacts/prepare_callbacks/checkpoint_dir
+        create_directories([
+            Path(model_ckpt_dir), # artifacts/prepare_callbacks/checkpoint_dir
+            Path(config.tensorboard_root_log_dir) # create artifacts/prepare_callbacks/tensorboard_log_dir
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+        return prepare_callback_config
