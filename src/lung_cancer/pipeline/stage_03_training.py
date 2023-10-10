0404e42e5b821ad2e7b88710f65b9778729f553e
Eslam-shaban <73853163+Eslam-shaban@users.noreply.github.com>
2023-10-10 21:04:06 +0300

    DVC Added

--- src/lung_cancer/pipeline/stage_03_training.py
+++ src/lung_cancer/pipeline/stage_03_training.py
@@ -18,14 +18,11 @@ class ModelTrainingPipeline:
         prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
         callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
 
-
         training_config = config.get_training_config()
         training = Training(config=training_config)
         training.get_base_model()
-        training.train_valid_generator()
-        training.train(
-            callback_list=callback_list
-        )
+        training.train_valid_test_generator()
+        training.train(callback_list=callback_list)
 
 
 
